956ec0278e2a784ef382304e7e51f63f6296b212
Amrendra Kumar <Amrendra_Kumar@skillsoft.com>
2016-12-29 16:37:12 -0500

    temp fix for image preview. Could not find a way to use the full screen due to relative position of md-sidenav-container. checking-in missing file from the last check-in

---
+++ src/app/services/app.class.ts
@@ -0,0 +1,74 @@
+
+export class Args {
+    year: number;
+    type: string;
+}
+
+export class Members {
+    year: number;
+    title: string;
+    memberList: Member[];
+}
+
+export class Member {
+    membershipType: string;
+    membershipTerm?: string;
+    fullname: string;
+    lastname: string;
+    email?: string;
+    contact?: string;
+    title?: string;
+    subtitle?: string;
+}
+
+export class MemberList {
+    year: number;
+    executives: Member[];
+    boardMembers: Member[];
+    regularMembers: Member[];
+}
+
+export class Photo {
+    rows: number;
+    caption: string;
+    thumbnail?: string;
+    imageUrl: string;
+}
+
+export class Gallery {
+    year: number;
+    section: string;
+    title: string;
+    subtitle: string;
+    row_height: number;
+    max_image_width: number;
+    poster: string;
+    photos: Photo[];
+    hasMultiple?: boolean;
+}
+
+export class Event {
+    key: string;
+    title: string;
+    date: string;
+    time: string;
+    venue: string;
+    address?: string;
+    person?: string;
+    contact?: string;
+    email?: string;
+    subject?: string;
+    ps?: string;
+    eventUrl?: string;
+}
+
+export class AnnouncementBar {
+    interval: number;
+    annoucements: Announcement[];
+}
+
+export class Announcement {
+    title: string;
+    desc: string;
+    url: string;
+}
