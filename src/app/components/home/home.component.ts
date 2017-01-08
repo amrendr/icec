7bc48d2ece184a6df1425d9c9add98ea32bb5971
Amrendra Kumar <Amrendra_Kumar@skillsoft.com>
2017-01-08 17:53:39 -0500

    added homepage tagline and added some of the gallery card.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Args } from '../../services/app.class';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  indiafest: Args = { type: 'indiafest', year: null };
+  youth: Args = { type: 'youth', year: null };
+  resources: Args = { type: 'resources', year: null };
 
   constructor() { }
 
