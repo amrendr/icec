85857ba27f84dd8906636c361752916b0062ef75
Amrendra Kumar <Amrendra_Kumar@skillsoft.com>
2017-04-08 23:12:47 -0400

    upgraded to   angular-cli 1.0.0   angular 4.0.1   material beta3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -60,6 +61,7 @@ import { TrapFocusDirective } from './app.trap-focus.directive';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     MaterialModule,
---
+++ src/app/icec-material.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { MdSelectModule, MdInputModule, MdButtonModule, MdToolbarModule,
+         MdIconModule, MdGridListModule, MdRadioModule, MdSidenavModule, 
+         MdListModule, MdCardModule, MdRippleModule, MdProgressBarModule,
+         MdLineModule, MdOptionModule } from '@angular/material';
+
+@NgModule({
+  exports: [
+    MdSelectModule,
+    MdInputModule,
+    MdButtonModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdGridListModule,
+    MdRadioModule,
+    MdSidenavModule,
+    MdListModule,
+    MdCardModule,
+    MdRippleModule,
+    MdLineModule,
+    MdProgressBarModule,
+    MdOptionModule,
+
+  ],
+  declarations: []
+})
+export class IcecMaterialModule { }
